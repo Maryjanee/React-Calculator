e257f8309ea9b3ad727cde120a9e9a595162346a
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-01-14 11:01:47 +0100

    Fix Linter issues and unnecessary files and assets

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,3 @@
+/* eslint-disable no-unused-vars */
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
