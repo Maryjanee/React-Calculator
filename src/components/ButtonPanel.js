495480dca020ba3a20204b0f8f40239e0ee2efe1
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-01-20 12:00:43 +0100

    Refactor ButtonPanel component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,22 +2,30 @@ import PropTypes from 'prop-types';
 import Button from './Button';
 import '../styles/ButtonPanel.css';
 
-const ButtonPanel = props => (
+const Panel = ({ onClick, symbols }) => symbols.map((symbol, index) => (
+  <Button
+    key={(symbol + index).toString()}
+    buttonName={symbol}
+    onClick={onClick}
+  />
+));
+
+const ButtonPanel = ({ onClick }) => (
   <div className="btn-panel">
     <div className="btn-group">
-      {['AC', '+/-', '%', '÷'].map(((btnVal, index) => <Button key={(btnVal + index).toString()} buttonName={btnVal} onClick={props.onClick} />))}
+      <Panel onClick={onClick} symbols={['AC', '+/-', '%', '÷']} />
     </div>
     <div className="btn-group">
-      {['7', '8', '9', '*'].map(((btnVal, index) => <Button key={(btnVal + index).toString()} buttonName={btnVal} onClick={props.onClick} />))}
+      <Panel onClick={onClick} symbols={['7', '8', '9', '*']} />
     </div>
     <div className="btn-group">
-      {['4', '5', '6', '-'].map(((btnVal, index) => <Button key={(btnVal + index).toString()} buttonName={btnVal} onClick={props.onClick} />))}
+      <Panel onClick={onClick} symbols={['4', '5', '6', '-']} />
     </div>
     <div className="btn-group">
-      {['1', '2', '3', '+'].map(((btnVal, index) => <Button key={(btnVal + index).toString()} buttonName={btnVal} onClick={props.onClick} />))}
+      <Panel onClick={onClick} symbols={['1', '2', '3', '+']} />
     </div>
     <div className="btn-group last">
-      {['0', '.', '='].map(((btnVal, index) => <Button key={(btnVal + index).toString()} buttonName={btnVal} onClick={props.onClick} />))}
+      <Panel onClick={onClick} symbols={['0', '.', '=']} />
     </div>
   </div>
 );
