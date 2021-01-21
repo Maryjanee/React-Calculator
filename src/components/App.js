0715803b6634cbf37ba364c2054b3382a8102541
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-01-21 17:59:00 +0100

    Refactor App component to use useState Hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,25 @@
 /* eslint-disable no-sequences */
-import React from 'react';
+import React, { useState } from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import '../styles/App.css';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = buttonName => {
-      const { total, next, operation } = this.state;
-      this.setState(calculate({ total, next, operation }, buttonName));
-    };
-  }
+const App = () => {
+  const [state, setState] = useState({ total: null, next: null, operation: null });
 
-  render() {
-    const { total, next } = this.state;
-    return (
-      <div className="calculator">
-        <Display result={next || total || 0} />
-        <ButtonPanel onClick={this.handleClick} />
-      </div>
-    );
-  }
-}
+  const handleClick = buttonName => {
+    const { total, next, operation } = state;
+    setState(calculate({ total, next, operation }, buttonName));
+  };
+
+  const { total, next } = state;
+  return (
+    <div className="calculator">
+      <Display result={next || total || 0} />
+      <ButtonPanel onClick={handleClick} />
+    </div>
+  );
+};
 
 export default App;
