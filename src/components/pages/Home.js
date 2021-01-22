94e293fd9a03a2e96228ca4bd30b682a83aad722
Maryjane Akpagbue <akpagbuemaryjane@gmail.com>
2021-01-22 11:10:08 +0100

    Install react-router-dom and create pages for app

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const Home = () =>{
+  return (
+    <div>
+      <div>This is the Home Page</div>
+      <p>Hello From Home</p>
+    </div>
+  )
+}
+
+export default Home;
